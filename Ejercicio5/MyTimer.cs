124872fc379b382b11a63ea7573cca30ed07c238
Hugo Mendez <hugo3011mendez@gmail.com>
2020-11-19 11:44:03 +0100

    - Añadidos comentarios a la clase MyTimer que contienen el enunciado del ejercicio, para tenerlos siempre cerca.
    - Añadidas funciones, constructor y delegado, aún sin programar.

--- Ejercicio5/MyTimer.cs
+++ Ejercicio5/MyTimer.cs
@@ -6,8 +6,31 @@ using System.Threading.Tasks;
 
 namespace Ejercicio5
 {
+    // • Tendrá una propiedad Intervalo que indica en ms cada cuánto se ejecuta una determinada función.
+    // • Deberás crear un delegado para poder pasarle una función que será la que ejecute.
+    // • El Constructor tendrá como parámetro la función que se le pasa. Se recomienda en este constructor lanzar el hilo de ejecución aunque este entre en espera.
+    // • Tendrá un método run el cual inicia la ejecución de la función cada Intervalo ms.
+    // • Tendrá un método pause que pausa la ejecución.
+
     class MyTimer
     {
+        public delegate void Delegado();
 
+        int intervalo;
+
+        public void run()
+        {
+
+        }
+
+        public void pause()
+        {
+
+        }
+
+        public MyTimer()
+        {
+
+        }
     }
 }
